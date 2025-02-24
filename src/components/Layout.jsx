5ceeee0ca953fde4d359d586f7612ce1215a1a67
saadghori <saadghori33@gmail.com>
2025-02-24 00:30:51 -0500

    Fixing deployment issue related to navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,11 @@
 // components/Layout.js
 import Footer from './Footer';
-import Navbar from './Navbar';
+import NavBar from './NavBar';
 
 const Layout = ({ children }) => {
   return (
     <div className="d-flex flex-column min-vh-100">
-      <Navbar />
+      <NavBar />
       <main className="flex-grow-1">{children}</main>
       <Footer />
     </div>
