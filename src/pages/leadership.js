7d1bcf230e20ee4834dd6fb5e1acac31474324e5
saadghori <saadghori33@gmail.com>
2025-02-24 00:44:40 -0500

    Adding pages

---
+++ src/pages/leadership.js
@@ -0,0 +1,12 @@
+// pages/leadership.js
+const Leadership = () => {
+    return (
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-blue-600"> Leadership </h1>
+        <p className="text-lg mt-4">Optimus Pharma management info will go here.</p>
+      </div>
+    );
+  };
+  
+  export default Leadership;
+  
---
+++ src/pages/products.js
@@ -0,0 +1,12 @@
+// pages/products.js
+const Products = () => {
+    return (
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-blue-600">Products</h1>
+        <p className="text-lg mt-4">Optimus Pharma products will go here.</p>
+      </div>
+    );
+  };
+  
+  export default Products;
+  
