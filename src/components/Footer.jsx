d667f6f7b9a4d0dce53908ec4e2de0a479619375
saadghori <saadghori33@gmail.com>
2025-02-26 01:47:49 -0500

    Improved Footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,16 @@
 // components/Footer.js
-import { Container, Row, Col, Nav, Stack, NavLink } from 'react-bootstrap';
-import styles from '@/styles/Footer.module.css'; // Import the CSS module
+import { Container, Row, Col, Nav } from 'react-bootstrap';
+import styles from '@/styles/Footer.module.css'; // Assuming a CSS module for styling
 import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <Container fluid>
-        <Row className='bg-gray text-black p-4 g-4'>
-          <Col md={5} className='ps-lg-5'>
-            <Stack>
+        <Row className="bg-gray text-black p-4 g-5">
+          {/* Logo and Slogan Column */}
+          <Col md={4}>
+            <div>
               <img
                 src="/logo-optimus.png"
                 alt="Optimus Pharma Logo"
@@ -19,11 +20,13 @@ export default function Footer() {
               <p className={styles.slogan}>
                 Unwavering commitment to quality, innovation, and accessibility.
               </p>
-            </Stack>
+            </div>
           </Col>
-          <Col md={3}>
+
+          {/* Useful Links Column */}
+          <Col md={2}>
             <Nav className="flex-column">
-              <h4 className={styles.heading}>Useful Links</h4>
+              <h4 className={styles.heading}>Quick Links</h4>
               <Link href="/" passHref legacyBehavior>
                 <Nav.Link className={styles.navLink}>Home</Nav.Link>
               </Link>
@@ -33,8 +36,8 @@ export default function Footer() {
               <Link href="/products" passHref legacyBehavior>
                 <Nav.Link className={styles.navLink}>Products</Nav.Link>
               </Link>
-              <Link href="/media" passHref legacyBehavior>
-                <Nav.Link className={styles.navLink}>Media</Nav.Link>
+              <Link href="/leadership" passHref legacyBehavior>
+                <Nav.Link className={styles.navLink}>Leadership</Nav.Link>
               </Link>
               <Link href="/contact" passHref legacyBehavior>
                 <Nav.Link className={styles.navLink}>Contact Us</Nav.Link>
@@ -42,21 +45,40 @@ export default function Footer() {
             </Nav>
           </Col>
 
-          <Col md={4}>
-            <h4 className={styles.heading} >Head Office</h4>
+          {/* Contact Column */}
+          <Col md={3}>
+            <h4 className={styles.heading}>Contact</h4>
             <div className={styles.headOffice}>
-                <p>Optimus Pharma Pvt. (Ltd.)</p>
-                <p>259 F/I-Block, Wapda Town, Lahore</p>
-                <p>Cell: +92 321 5182261</p>
-                <p>Tel: +92 42 35186994</p>
-                <p>Fax: +92 42 32212467</p>
-                <p>E-mail: info@optimuspharma.com</p>
+              <p>Optimus Pharma Pvt. (Ltd.)</p>
+              <p>259 F/I-Block, Wapda Town, Lahore</p>
+              <p>Cell: +92 321 5182261</p>
+              <p>Tel: +92 42 35186994</p>
+              <p>Fax: +92 42 32212467</p>
+              <p>E-mail: info@optimuspharma.com</p>
             </div>
+          </Col>
+
+          {/* Group Companies Column */}
+          <Col md={3}>
+            <Nav className="flex-column">
+              <h4 className={styles.heading}>Group</h4>
+              <Nav.Link
+                href="https://optimusentrepot.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.navLink}
+              >
+                Optimus Entrepot ⧉
+              </Nav.Link>
+            </Nav>
           </Col>
         </Row>
         <Row className="bg-gray p-2">
           <Col>
-            <small className="text-muted" style={{ fontSize: '12px', textAlign: 'left', display: 'block' }}>
+            <small
+              className="text-muted"
+              style={{ fontSize: '12px', textAlign: 'left', display: 'block' }}
+            >
               © 2025 Optimus Pharma. All rights reserved.
             </small>
           </Col>
