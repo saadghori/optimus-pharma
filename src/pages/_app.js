9c7ce897be5affc4a888e9ec8cc2f0c9b1de4bad
saadghori <saadghori33@gmail.com>
2025-02-26 17:18:33 -0500

    Fixed viewport issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,7 +9,7 @@ function App({ Component, pageProps }) {
       <Head>
         <meta 
           name="viewport" 
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"
         />
       </Head>
       <Component {...pageProps} />
