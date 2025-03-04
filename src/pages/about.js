486d3a69ddf81e7da46767fbc8510dd5107cfb54
saadghori <saadghori33@gmail.com>
2025-03-04 18:58:13 -0500

    Adjusted hero image overlay (about)

--- src/pages/about.js
+++ src/pages/about.js
@@ -48,7 +48,7 @@ const About = () => {
         buttonText="OUR LEADERSHIP"
         overlayOpacity={0.55}
         buttonLink="/leadership"
-        overlayColor = 'rgb(16, 16, 16, 0.60)'
+        overlayColor = 'rgba(20, 35, 60, .82)'
       />
 
       {/* Image Text Section */}
